b8c0a6bb89417661e459c3eaf8b516715f64ae60
Luisa Camila Cordoba <luisita.camila7@gmail.com>
2025-08-29 22:21:06 -0500

    Add files via upload

---
+++ ejercicio1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void main(String args[]) {
+        int opcion = 0;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Escoja su opcion");
+        opcion = entrada.nextInt();
+        System.out.println("TU SOLO DIME SI TE QUEDAS O TE VAAAAAS AAAAAAAAS AAAAAS");
+    }
+
+}
